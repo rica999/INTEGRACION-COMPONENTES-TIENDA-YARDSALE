0140b8c513a5d8a637b15f840b7c4d378d9a0f67
Ricardo Chavez <rachav999iron@gmail.com>
2023-03-25 08:45:37 -0500

    INTEGRACIÓN MENÚ MOBILE CON NAVBAR

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-const iconArrow = document.querySelector("#icon-arrow");
+const iconArrow = document.querySelector("#iconArrow");
 const menuNav = document.querySelector("#menuNav");
+
+const iconMenu = document.querySelector("#iconMenu");
+const menuMobile = document.querySelector("#menuMobile");
+const iconClose = document.querySelector("#iconClose");
 
 iconArrow.addEventListener("click",toggleMenuNav);
 
 function toggleMenuNav(){
     menuNav.classList.toggle("hidden");
+}
+
+iconMenu.addEventListener("click",openMenuMobile);
+iconClose.addEventListener("click",closeMenuMobile);
+
+function openMenuMobile(){
+    menuMobile.classList.remove("hidden");
+}
+
+function closeMenuMobile(){
+    menuMobile.classList.add("hidden");
 }
