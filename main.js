61cf7fba293f1043e21303d8a642994c21613b7f
Ricardo Chavez <rachav999iron@gmail.com>
2023-03-25 16:26:06 -0500

    INTEGRACIÓN SHOOPING CAR

--- main.js
+++ main.js
@@ -5,10 +5,18 @@ const iconMenu = document.querySelector("#iconMenu");
 const menuMobile = document.querySelector("#menuMobile");
 const iconClose = document.querySelector("#iconClose");
 
+const iconShop = document.querySelector("#iconShop");
+const orderProducts = document.querySelector("#orderProducts");
+const iconMenuShopCar = document.querySelector("#iconMenuShopCar");
+const iconArrowShopCar = document.querySelector("#iconArrowShopCar");
+
 iconArrow.addEventListener("click",toggleMenuNav);
 
 function toggleMenuNav(){
     menuNav.classList.toggle("hidden");
+    if(!orderProducts.classList.contains("hidden")){
+        orderProducts.classList.add("hidden");
+    };
 }
 
 iconMenu.addEventListener("click",openMenuMobile);
@@ -20,4 +28,20 @@ function openMenuMobile(){
 
 function closeMenuMobile(){
     menuMobile.classList.add("hidden");
+}
+
+iconShop.addEventListener("click",openOrderProducts);
+
+function openOrderProducts(){
+    orderProducts.classList.toggle("hidden");
+    if(menuNav.classList.contains("hidden")){
+        menuNav.classList.add("hidden");
+    };
+}
+
+iconMenuShopCar.addEventListener("click",closeShopCar);
+iconArrowShopCar.addEventListener("click",closeShopCar);
+
+function closeShopCar(){
+    orderProducts.classList.toggle("hidden");
 }
